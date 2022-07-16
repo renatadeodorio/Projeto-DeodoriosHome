9a8db570f6e6240b523b193bc4107bc5619452b6
Renata Deodório <renatadeod@gmail.com>
2022-07-15 22:43:59 -0300

    Seeders

--- deodorioshome-laravel/app/Models/User.php
+++ deodorioshome-laravel/app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'remember_token',
+        'email_verified_at'
     ];
 
     /**
@@ -33,12 +35,4 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
