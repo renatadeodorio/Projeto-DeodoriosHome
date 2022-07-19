3dd9bf0cf6a6150abcd119966487de1761ed9966
Renata Deodório <renatadeod@gmail.com>
2022-07-19 16:59:08 -0300

    Excluir Usuário

--- deodorioshome-laravel/resources/views/users/show.blade.php
+++ deodorioshome-laravel/resources/views/users/show.blade.php
@@ -9,7 +9,8 @@
       <th scope="col">Nome</th>
       <th scope="col">Email</th>
       <th scope="col">Data Cadastro</th>
-      <th scope="col">Ações</th>
+      <th scope="col">Visualizar</th>
+      <th scope="col">Deletar</th>
     </tr>
   </thead>
   <tbody class="text-center">
@@ -20,9 +21,16 @@
          <td>{{ date('d/m/Y- H:i', strtotime($user->created_at)) }}</td>
          <td>
             <a href="{{ route('users.edit', $user->id) }}" class="btn btn-warning text-white">Editar</a>
-            <a href="" class="btn btn-danger text-white">Deletar</a>
-        </td>
-    </tr>
+         </td>
+          <td>
+             <form action="{{ route('users.destroy', $user->id) }}" method="POST">
+               @method('DELETE')
+               @csrf
+               <button type="submit" class="btn btn-danger text-white">Deletar</button>
+            </form>
+           </td>
+
+        </tr>
   </tbody>
 </table>
  @endsection
--- deodorioshome-laravel/routes/web.php
+++ deodorioshome-laravel/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\{
     ViaCepController
 };
 
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
