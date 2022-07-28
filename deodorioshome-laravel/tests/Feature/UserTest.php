50dd190c9d063d6cdafd34577d27b12b3a84eb09
Renata Deodório <renatadeod@gmail.com>
2022-07-28 15:51:53 -0300

    Testes1

--- deodorioshome-laravel/tests/Feature/UserTest.php
+++ deodorioshome-laravel/tests/Feature/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,8 +14,17 @@ class UserTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_user()
     {
+        $user = User::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        $this->actingAs($user);
+
         $response = $this->get('/users');
 
         $response->assertStatus(200);
--- deodorioshome-laravel/tests/Unit/UserTest.php
+++ deodorioshome-laravel/tests/Unit/UserTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-    public function test_that_true_is_true()
+    public function test_user()
     {
         $this->assertTrue(true);
     }
