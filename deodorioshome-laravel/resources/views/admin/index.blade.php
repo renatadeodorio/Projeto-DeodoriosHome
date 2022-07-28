f56b722b5b635b7c9cace6e7d8996d807bfebc47
Renata Deodório <renatadeod@gmail.com>
2022-07-27 22:31:05 -0300

    Ajustes

--- deodorioshome-laravel/resources/views/admin/index.blade.php
+++ deodorioshome-laravel/resources/views/admin/index.blade.php
@@ -5,8 +5,8 @@
 <div class="card mt-3" style="width: 22rem;">
      <img src="{{ asset('storage/profile/dash.jpg') }}" class="card-img-top" alt="Dash">
      <div class="card-body-center">
-       <h5 class="card-title">Bem vindo(a) à Deodorio's Home </h5>
-       <p class="card-text">Quadros que contam histórias</p>
+       <h4 class="card-title">Seja Bem vindo(a)! </h4>
+       <p class="card-text"> ✰ Quadros que contam histórias ✰</p>
      </div>
  </div>
 @endsection
