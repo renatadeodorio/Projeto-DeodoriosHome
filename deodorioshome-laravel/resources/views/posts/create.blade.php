816b5ce41fd26ab36b1bfa7c528e5533d7feedae
Renata Deodório <renatadeod@gmail.com>
2022-08-02 23:08:24 -0300

    Ajustes Quadros

---
+++ deodorioshome-laravel/resources/views/posts/create.blade.php
@@ -0,0 +1,43 @@
+@extends('template.users')
+@section('title', 'Novo Quadro')
+@section('body')
+    <h1>Novo Quadro</h1>
+
+  @if($errors->any())
+     <div class="alert alert-danger" role="alert">
+     @foreach($errors->all() as $error)
+            {{ $error }}<br>
+     @endforeach
+     </div>
+
+    @endif
+    <form action="{{ route('posts.create') }}" method="POST" enctype="multipart/form-data">
+       @csrf
+      <div class="mb-3">
+        <label for="name" class="form-label">Quadros</label>
+        <input type="text" class="form-control" id="name" name="name" aria-describedby="Nome">
+      </div>
+      <div class="mb-3">
+        <label for="email" class="form-label">Descrição</label>
+        <input type="email" class="form-control" id="email" name="email">
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Temas</label>
+        <input type="password" class="form-control" id="password" name="password">
+      </div>
+      <label for="password" class="form-label">Formatos</label>
+        <input type="password" class="form-control" id="password" name="password">
+      </div>
+      <label for="password" class="form-label">Tamanhos</label>
+        <input type="password" class="form-control" id="password" name="password">
+      </div>
+      <label for="password" class="form-label">Preços</label>
+        <input type="password" class="form-control" id="password" name="password">
+      </div>
+      <div class="mb-3">
+          <label for="image" class="form-label">Selecione uma Imagem</label>
+          <input type="file" class="form-control form control-md" id="image" name="image" />
+      </div>
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+    @endsection
