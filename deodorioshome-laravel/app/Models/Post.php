34cba0453746666ac448e26bebb8e3404bace4bd
Renata Deodório <renatadeod@gmail.com>
2022-08-02 23:04:20 -0300

    Ajustes Posts

--- deodorioshome-laravel/app/Models/Post.php
+++ deodorioshome-laravel/app/Models/Post.php
@@ -8,9 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable = [
+     'Id',
+     'Quadros',
+     'Descrição',
+     'Temas',
+     'Formatos',
+     'Tamanhos',
+     'Artistas',
+     'Preços'
+    ];
+
+    public function getPosts(string $search = null)
+    {
+        $posts = $this->where( function ($query) use ($search) {
+            if($search) {
+                $query->where('temas', 'LIKE',"%{$search}%");
+                $query->orWhere('quadros', 'LIKE',"%{$search}%");
+            }
+        })
+        ->paginate(6);
+        return $posts;
+    }
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
 }
